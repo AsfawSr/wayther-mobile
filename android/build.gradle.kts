f99615c23cdc5f078a0b339d4f417588ef20f077
Asfaw <asfawyemane21@gmail.com>
2026-06-25 16:00:10 +0300

    feat: upgrade geocoding dependency to version 4.0.0 and update android build configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,16 +20,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-// Force all plugin sub-projects (libraries) to compile against SDK 36.
-// Required because plugins like geocoding_android ship with compileSdk = 33,
-// which is incompatible with androidx.exifinterface:exifinterface:1.4.1+.
-subprojects {
-    afterEvaluate {
-        extensions.findByType(com.android.build.gradle.LibraryExtension::class.java)
-            ?.apply { compileSdk = 36 }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
